61746d21f3b7ae3b07a195095994cb7ad29d4db4
Black1499 <liu1499@foxmail.com>
2019-01-15 10:45:38 +0800

    第一次起草

---
+++ backstage/src/test/java/com/nf147/platform/PlatformApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nf147.platform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlatformApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
